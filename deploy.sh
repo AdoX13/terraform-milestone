7bbd00af752e3260c533486a219aae09dfbe057d
Adelin Popa <adelin.popa@Adelins-MacBook-Pro.local>
2022-10-04 16:54:07 +0300

    update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
 env=$1
+action=$2
 
-if [[ "$env" != "DEV" && "$env" != "PROD" ]]; then
-	echo "Usage: ./deploy.sh DEV/PROD"
+if [[ "$env" != "DEV" && "$env" != "PROD" && "$action" != "create" && "$action" != "destroy" ]]; then
+	echo "Usage: ./deploy.sh DEV/PROD create/destroy"
 	exit 1
 fi
 
-cd live/$env/network
-terraform init
-terraform apply -auto-approve
-
-cd ../data-storage
-terraform init
-terraform apply -auto-approve
-
-cd ../services
-terraform init
-terraform apply -auto-approve
+if [[ "$action" == "create" ]]
+then
+	# Deploy network
+	cd live/$env/network
+	terraform init
+	terraform apply -auto-approve
+	# Deploy data-storage
+	cd ../data-storage
+	terraform init
+	terraform apply -auto-approve
+	# Deploy services
+	cd ../services
+	terraform init
+	terraform apply -auto-approve
+else
+	# Destroy services
+	cd live/$env/services
+	terraform apply -destroy -auto-approve
+	# Destroy data-storage
+	cd ../data-storage
+	terraform apply -destroy -auto-approve
+	# Dstroy network
+	cd ../network
+	terraform apply -destroy -auto-approve
+fi
